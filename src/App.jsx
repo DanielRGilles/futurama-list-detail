c50f8bdd76cfb1ddbd83a3d9365080be7e1409d9
DanielRGilles <danryangilles@gmail.com>
2022-01-10 18:28:25 -0800

    initial layout and api function

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,24 @@
+import { Route, Switch, Link } from "react-router-dom";
+import Header from "./components/Layout/Header";
+import Home from "./views/Home/Home";
+import List from "./views/List/List";
+
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Header/>
+      <Switch>
+        <Route path='/'>
+          <Home/>
+        </Route >
+        <Route path='/characters'>
+          <List/>
+        </Route>
+        <Route path='/characters/:id'>
+          <Detail/>
+        </Route>
+      </Switch>
+      </>
+  )
 }
